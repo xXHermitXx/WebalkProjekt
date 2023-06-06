5e9da5398df748fe4040e9950938358923c02981
jakvince <113837967+jakvince@users.noreply.github.com>
2023-06-06 19:42:42 +0200

    Add files via upload

---
+++ src/main/java/org/example/webapp/WebalkProjekt/Components/Navbar_H.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Box, Container, Flex, Link} from "@chakra-ui/react";
+
+const Navbar_H = () => {
+  return (
+    <Box bg="gray.900" py={4}>
+      <Container ml={5}>
+        <Flex align="Center" justify="flex-start">
+          <Link color="white" fontSize="xl" fontWeight="bold" href="#">
+            Egyetemi Honlap
+          </Link>
+          <Flex ml={4}>
+            <Link color="white" mr={4} href="#">
+              Kezdőlap
+            </Link>
+            <Link color="white" mr={4} href="#">
+              Tárgyak
+            </Link>
+            <Link color="white" href="#">
+              Profil
+            </Link>
+          </Flex>
+        </Flex>
+      </Container>
+    </Box>
+  );
+};
+
+export default Navbar_H;
