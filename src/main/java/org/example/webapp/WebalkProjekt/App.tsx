8ee8052ac7867a50ffbd1f84ca957c98a07b7771
jakvince <113837967+jakvince@users.noreply.github.com>
2023-06-06 19:44:40 +0200

    Update App.tsx

--- src/main/java/org/example/webapp/WebalkProjekt/App.tsx
+++ src/main/java/org/example/webapp/WebalkProjekt/App.tsx
@@ -1,14 +1,12 @@
 import React from "react"
-import Header from "./Components/header";
-import Filters from "./Components/filters";
-import DataTable from "./Components/table";
-import {Container} from "@chakra-ui/react";
-
+import Navbar_H from "./Components/Navbar_H";
+import Navbar_O from "./Components/Navbar_O";
+import Tfelvetel from "./Pages/targyfelvetel";
+import Jegybeiras from "./Pages/jegybeiras";
 
 export const App = () => (
   <>
-  <Header />
-    <Filters />
-    <DataTable />
+  <Navbar_H />
+   <Tfelvetel />
   </>
 )
